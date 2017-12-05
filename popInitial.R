429351de432ed96170fc82b3f94adb00b9068dd7
QinganZhao <qingan_zhao@berkeley.edu>
2017-12-05 01:17:38 -0800

    update v12.5

---
+++ popInitial.R
@@ -0,0 +1,29 @@
+##### support function 1: popInitial() #####
+
+# this function intializes the population
+popInitial <- function(popNum = 100, geneLength, zeroRate = 0){
+  #input: 
+  #   popNum: population number (positive integer)
+  #   geneLength: gene length (positive integer)
+  #   zeroRate: the rate of zero among the genes of the first generation
+  #output: populations of the first generation with their genes (0/1)
+  
+  if(popNum < 1 | round(popNum) != popNum){
+    stop("Population number must be a positive integer.")
+  }
+  if(geneLength < 1 | round(geneLength) != geneLength){
+    stop("Gene length must be a positive integer.")
+  }
+  if(zeroRate < 0 | zeroRate > 1){
+    stop("Zero rate must be within [0,1].")
+  }
+  
+  #determine the numbers of zeros and ones based on the zeroRate
+  geneNum <- popNum * geneLength
+  zeroNum <- round(geneNum * zeroRate)
+  oneNum <- geneNum - zeroNum 
+  
+  #return the first generation
+  return(matrix(sample(c(rep(0, zeroNum), rep(1, oneNum)), geneNum), 
+                nrow = popNum, ncol = geneLength))
+}

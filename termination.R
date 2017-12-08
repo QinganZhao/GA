9d5fd57bacc16874fbad667fe1e860bbe957a734
Mia-Zhong <miazhongrj@gmail.com>
2017-12-08 13:51:20 -0800

    termination built into select.r

--- termination.R
+++ termination.R
@@ -3,40 +3,16 @@
 
 # t: number of generations
 # t_last: the last t_last generations for convergence check, default as 15
+# t_max: the upperbound limit of generations
 
+# two ways of determining convergence:
+# running mean
 approxEqual = function(a,b){
   abs(a - b) < .Machine$double.eps * abs(a + b)
 }
 
-
-select <- function(X, Y, popNum = 100, t_last = 15, .......){
-  # setup
-  
-  # initialization
-  
-  # evaluation
-  
-  # termination
-  t = 1
-  min_record <- array(dim = 0)
-  min_record <- c(min_record, min(df_current$fitness))
-  
-  while(t < t_last){
-    # ranking
-    # choosing
-    # crossover
-    # mutation
-    
-    t = t + 1
-  }
-  
-  while(approxEqual(min(df_init$fitness), mean(min_record[(t - t_last + 1):t])) = FALSE){
-    # ranking
-    # choosing
-    # crossover
-    # mutation
-    t = t + 1
-    }
-}
+# the convergence on alleles
+# cvgRate_allele: the proportion of genes on one allele that are the same
+# cvgRate_chrom: the proportion of alleles that are converged
 
 

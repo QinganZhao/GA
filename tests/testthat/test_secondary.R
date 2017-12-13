032d7480815fa3e010ea420b5f2575a4cdb55604
Côme de Lassus <31293792+ComedeLassus@users.noreply.github.com>
2017-12-12 17:26:26 -0800

    minor changes again

--- tests/testthat/test_secondary.R
+++ tests/testthat/test_secondary.R
@@ -4,6 +4,7 @@ test_that('function evaluation gives correct result',{
   Y <- c(5,6)
   gen <- matrix(c(0,1,0,1,0,0,1,1),ncol = 2)
   E <- evaluation(X,Y,gen,4)
+
   expect_equal(E$fitness,rep(-Inf,4))
 
   #more complex data
@@ -13,11 +14,57 @@ test_that('function evaluation gives correct result',{
   }
   Y <- 1+2*X[,1]-3*X[,2]+0.0001*X[,3]
   Gen <- upper.tri(matrix(1,6,6),diag = TRUE)
-  expected <- c(-357.23560,-361.40398,-363.32859,-371.79320,-362.12230,22.50741)
+  expected <- c(-357.23560,-361.40398,-363.32859,
+                -371.79320,-362.12230,22.50741)
   E <- evaluation(X,Y,Gen,6)
   expect_true(all.equal(E$fitness,expected,tolerance = 10^-3))
 })
 
-test_that('function breed behaves correctly',{
+test_that('function superEvaluation gives correct result',{
+  #very simple data
+  X <- matrix(c(1,2,3,4),ncol = 2)
+  Y <- c(5,6)
+  gen <- matrix(c(0,1,0,1,1,0,1,1),ncol = 2)
+  E <- superEvaluation(X,Y,gen,4)
+  rownames(E$fitness) <- NULL
 
+  expect_equal(as.vector(E$fitness),rep(-Inf,4))
+
+  #more complex data
+  X <- data.frame(matrix(ncol = 6, nrow = 6))
+  for (i in 1:6){
+    for (j in 1:6){X[i,j]<- rep(3*i^2-i*j+7/j)}
+  }
+  Y <- 1+2*X[,1]-3*X[,2]+0.0001*X[,3]
+  Gen <- upper.tri(matrix(1,6,6),diag = TRUE)
+  expected <- c(-357.23560,-361.40398,-363.32859,
+                -371.79320,-362.12230,22.50741)
+  E <- superEvaluation(X,Y,Gen,6)
+  rownames(E$fitness) <- NULL
+
+  expect_true(all.equal(as.vector(E$fitness),expected,tolerance = 10^-3))
+})
+
+
+test_that('functions breed and popInitial behave correctly',{
+  #two identical generations should be unchanged if no mutation
+  Gen <- 1*upper.tri(matrix(1,3,3),diag = TRUE)
+  df <- as.data.frame(matrix(nrow = 3, ncol = 2))
+  df[1] <- apply(Gen, 1, paste, collapse = '')
+  df[2] <- apply(Gen, 1, paste, collapse = '')
+  M <- matrix(c(1,0,0,1,0,0,1,1,0,1,1,0,1,1,1,1,1,1),ncol = 3)
+  expect_equal(breed(df,mutation_prob = 0),M)
+
+  #without mutation, the number of 1-alleles must be constant
+  ch1 <- popInitial(10,10,0.5)
+  ch2 <- popInitial(10,10,0.2)
+  df <- as.data.frame(matrix(nrow = 10, ncol = 2))
+  df[1] <- apply(ch1, 1, paste, collapse = '')
+  df[2] <- apply(ch2, 1, paste, collapse = '')
+  expect_equal(sum(breed(df,mutation_prob = 0)),130)
+})
+
+test_that('function rankSelection behaves correctly',{
+  df <- data.frame(matrix(runif(10,0,10),ncol = 2))
+  expect_equal(rankSelection(df)$probability,c(1/3,4/15,1/5,2/15,1/15))
 })
